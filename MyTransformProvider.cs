d7c1989646fa601e0bfbbf34f45c2e5999f24732
yury-kozlov <glassiko@gmail.com>
2024-03-09 19:54:32 -0500

    init new project

---
+++ MyTransformProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Yarp.ReverseProxy.Transforms;
+using Yarp.ReverseProxy.Transforms.Builder;
+
+internal class MyTransformProvider : ITransformProvider
+{
+    public void ValidateRoute(TransformRouteValidationContext context)
+    {
+    }
+
+    public void ValidateCluster(TransformClusterValidationContext context)
+    {
+    }
+
+    public void Apply(TransformBuilderContext transformBuildContext)
+    {
+        transformBuildContext.AddRequestTransform(async ctx =>
+        {
+            await Task.Delay(1000); // simulate async
+            ctx.ProxyRequest.Headers.Add("my-source-url", ctx.HttpContext.Request.GetDisplayUrl());
+            ctx.ProxyRequest.RequestUri = new Uri($"http://localhost:8080/{DateTime.Now.Hour}/{DateTime.Now.Minute}/?sec={DateTime.Now.Second}");
+        });
+    }
+}
